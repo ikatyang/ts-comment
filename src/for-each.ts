a6a07f3fb6ecb1428eddff36a0e496d9fcbae8f6
ikatyang <ikatyang@gmail.com>
2017-07-26 00:14:53 +0800

    style: reformat with prettier

--- src/for-each.ts
+++ src/for-each.ts
@@ -3,14 +3,27 @@ import * as ts from 'typescript';
 // modified from https://github.com/eslint/typescript-eslint-parser/blob/f5fcc87/lib/convert-comments.js
 
 export type ForEachCallback = ForEachCallbackX<void | boolean>;
-export type ForEachCallbackX<T> = (comment: string, scanner: ts.Scanner, source_file: ts.SourceFile) => T;
+export type ForEachCallbackX<T> = (
+  comment: string,
+  scanner: ts.Scanner,
+  source_file: ts.SourceFile,
+) => T;
 
-export const for_each = (source: string | ts.SourceFile, callback: ForEachCallback) => {
-  const source_file = (typeof source === 'string')
-    ? ts.createSourceFile('', source, ts.ScriptTarget.Latest, false)
-    : source;
+export const for_each = (
+  source: string | ts.SourceFile,
+  callback: ForEachCallback,
+) => {
+  const source_file =
+    typeof source === 'string'
+      ? ts.createSourceFile('', source, ts.ScriptTarget.Latest, false)
+      : source;
   const source_text = source_file.getFullText();
-  const scanner = ts.createScanner(ts.ScriptTarget.Latest, false, ts.LanguageVariant.Standard, source_text);
+  const scanner = ts.createScanner(
+    ts.ScriptTarget.Latest,
+    false,
+    ts.LanguageVariant.Standard,
+    source_text,
+  );
 
   let token = scanner.scan();
   while (token !== ts.SyntaxKind.EndOfFileToken) {
@@ -28,10 +41,11 @@ export const for_each = (source: string | ts.SourceFile, callback: ForEachCallba
       }
       case ts.SyntaxKind.CloseBraceToken: {
         const container = get_node_container(source_file, start, end);
-        if (container && (
-            container.kind === ts.SyntaxKind.TemplateMiddle ||
-            container.kind === ts.SyntaxKind.TemplateTail
-          )) {
+        if (
+          container &&
+          (container.kind === ts.SyntaxKind.TemplateMiddle ||
+            container.kind === ts.SyntaxKind.TemplateTail)
+        ) {
           token = scanner.reScanTemplateToken();
           continue;
         }
@@ -40,7 +54,10 @@ export const for_each = (source: string | ts.SourceFile, callback: ForEachCallba
       case ts.SyntaxKind.SlashToken:
       case ts.SyntaxKind.SlashEqualsToken: {
         const container = get_node_container(source_file, start, end);
-        if (container && container.kind === ts.SyntaxKind.RegularExpressionLiteral) {
+        if (
+          container &&
+          container.kind === ts.SyntaxKind.RegularExpressionLiteral
+        ) {
           token = scanner.reScanSlashToken();
           continue;
         }
@@ -54,7 +71,11 @@ export const for_each = (source: string | ts.SourceFile, callback: ForEachCallba
   }
 };
 
-function get_node_container(source_file: ts.SourceFile, start: number, end: number) {
+function get_node_container(
+  source_file: ts.SourceFile,
+  start: number,
+  end: number,
+) {
   let container: undefined | ts.Node;
   find_container(source_file, start, end, target_container => {
     container = target_container;
@@ -62,18 +83,28 @@ function get_node_container(source_file: ts.SourceFile, start: number, end: numb
   return container;
 }
 
-function find_container(node: ts.Node, start: number, end: number, callback: (container: ts.Node) => void) {
+function find_container(
+  node: ts.Node,
+  start: number,
+  end: number,
+  callback: (container: ts.Node) => void,
+) {
   const node_start = node.pos;
   const node_end = node.end;
   if (start >= node_start && end <= node_end) {
     if (is_token(node)) {
       callback(node);
     } else {
-      ts.forEachChild(node, child_node => find_container(child_node, start, end, callback));
+      ts.forEachChild(node, child_node =>
+        find_container(child_node, start, end, callback),
+      );
     }
   }
 }
 
 function is_token(node: ts.Node) {
-  return node.kind >= ts.SyntaxKind.FirstToken && node.kind <= ts.SyntaxKind.LastToken;
+  return (
+    node.kind >= ts.SyntaxKind.FirstToken &&
+    node.kind <= ts.SyntaxKind.LastToken
+  );
 }
--- src/gets.ts
+++ src/gets.ts
@@ -1,17 +1,26 @@
 import * as ts from 'typescript';
-import {for_each, ForEachCallbackX} from './for-each';
+import { for_each, ForEachCallbackX } from './for-each';
 
 export type GetsCallback<T> = ForEachCallbackX<T>;
 
 export function gets(source: string | ts.SourceFile): string[];
-export function gets<T>(source: string | ts.SourceFile, callback: GetsCallback<T>): T[];
-export function gets(source: string | ts.SourceFile, callback: GetsCallback<any> = comment => comment): any[] {
+export function gets<T>(
+  source: string | ts.SourceFile,
+  callback: GetsCallback<T>,
+): T[];
+export function gets(
+  source: string | ts.SourceFile,
+  callback: GetsCallback<any> = comment => comment,
+): any[] {
   const results: any[] = [];
 
-  for_each(source, (comment: string, scanner: ts.Scanner, source_file: ts.SourceFile) => {
-    const result = callback(comment, scanner, source_file);
-    results.push(result);
-  });
+  for_each(
+    source,
+    (comment: string, scanner: ts.Scanner, source_file: ts.SourceFile) => {
+      const result = callback(comment, scanner, source_file);
+      results.push(result);
+    },
+  );
 
   return results;
 }
